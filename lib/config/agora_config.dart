5190da8a2efcf1c1125cac04bf8a75678649ff89
bidursapkota00 <bidursapkota00@gmail.com>
2024-10-17 17:05:46 +0545

    first commit

---
+++ lib/config/agora_config.dart
@@ -0,0 +1,5 @@
+class AgoraConfig {
+  static String token = '';
+  static String appId = 'fd873719d4e44cf9964136c0cbb4d031';
+  static String appcertificate = 'ea505c603a16429b99e11979b990e7b8';
+}
